04134c4feab6a95794dc755777466247f8c479d7
Bobby R. Ward <bobbyrward@gmail.com>
2013-08-01 09:42:47 -0400

    echo-server: read till eof.  allocate raw buffer

--- echo-server/echo-server.rs
+++ echo-server/echo-server.rs
@@ -4,6 +4,13 @@ use std::rt::io::net::tcp::*;
 use std::rt::io::net::ip::*;
 
 
+fn allocate_buffer(buffer_size: uint) -> ~[u8] {
+    let mut buffer: ~[u8] = std::vec::with_capacity(buffer_size);
+    unsafe { std::vec::raw::set_len(&mut buffer, buffer_size); }
+    return buffer
+}
+
+
 fn main() {
     let listen_address = Ipv4(127, 0, 0, 1, 8000);
 
@@ -17,12 +24,18 @@ fn main() {
     loop {
         let stream = Cell::new(listener.accept().unwrap());
         do spawn {
-            let mut buf = [1, ..512];
+
+            let mut dest_buffer = allocate_buffer(512);
             let mut stream = stream.take();
-            match stream.read(buf) {
-                Some(x) => stream.write(buf.slice_to(x)),
-                None => (),
-            };
+
+            loop {
+                match stream.read(dest_buffer) {
+                    Some(x) => stream.write(dest_buffer.slice_to(x)),
+                    None => {
+                        break
+                    },
+                };
+            }
         }
     }
 
